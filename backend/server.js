a0a0b55bb8611a8b5a99de7da0c8cda801ac58ef
ScriptSculptorJS <cmcunningham27@yahoo.com>
2025-08-07 18:41:38 -0700

    changepath.resolve to path.join

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(cors(corsOptions))
 app.use(express.static(path.join(_dirname, '..', 'frontend', 'build')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(_dirname, '..', 'frontend', 'build', 'index.html'))
+  res.sendFile(path.join(_dirname, '..', 'frontend', 'build', 'index.html'))
 })
 
 app.use('/api/users', userRoutes)
